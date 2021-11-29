a609ad4675cf172d4e65f82f53aa4980cd3de1df
Vitalii_Velich <silens088@gmail.com>
2021-11-29 21:23:59 +0300

    added final variable

--- src/test/java/vitalii/qa/files/DataTest.java
+++ src/test/java/vitalii/qa/files/DataTest.java
@@ -2,15 +2,15 @@ package vitalii.qa.files;
 
 public class DataTest {
 
-    public static String UPLOAD_FILE_PAGE_1 = "https://the-internet.herokuapp.com/upload";
-    public static String UPLOAD_FILE_PAGE_2 = "https://demoqa.com/upload-download";
+    final public static String UPLOAD_FILE_PAGE_1 = "https://the-internet.herokuapp.com/upload";
+    final public static String UPLOAD_FILE_PAGE_2 = "https://demoqa.com/upload-download";
 
-    public static String DOWNLOAD_FILE_PDF_PAGE = "https://file-examples.com/index.php/sample-documents-download/sample-pdf-download/";
-    public static String DOWNLOAD_FILE_XLS_PAGE = "https://file-examples.com/index.php/sample-documents-download/sample-xls-download/";
+    final public static String DOWNLOAD_FILE_PDF_PAGE = "https://file-examples.com/index.php/sample-documents-download/sample-pdf-download/";
+    final public static String DOWNLOAD_FILE_XLS_PAGE = "https://file-examples.com/index.php/sample-documents-download/sample-xls-download/";
 
-    public static String FILE1 = "pixel-art-barbute.jpg";
-    public static String FILE2 = "pixel-art-book.jpg";
+    final public static String FILE1 = "pixel-art-barbute.jpg";
+    final public static String FILE2 = "pixel-art-book.jpg";
 
-    public static String XLS_TEXT = "Rasheeda";
+    final public static String XLS_TEXT = "Rasheeda";
 
 }
